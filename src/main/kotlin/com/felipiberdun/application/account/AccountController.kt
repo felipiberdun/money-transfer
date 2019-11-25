0a27400e7d465cda9ec7d7f96379a6618a4b1fc8
Felipi Berdun <felipi.berdun@gmail.com>
2019-11-24 22:31:39 -0600

    Adds handler for Existent Account

--- src/main/kotlin/com/felipiberdun/application/account/AccountController.kt
+++ src/main/kotlin/com/felipiberdun/application/account/AccountController.kt
@@ -1,12 +1,9 @@
 package com.felipiberdun.application.account
 
-import com.felipiberdun.domain.accounts.AccountQuery
-import com.felipiberdun.domain.accounts.AccountService
-import com.felipiberdun.domain.accounts.CreateAccountCommand
-import com.felipiberdun.domain.accounts.toQuery
+import com.felipiberdun.domain.accounts.*
+import io.micronaut.http.*
 import io.micronaut.http.HttpResponse.*
-import io.micronaut.http.MediaType
-import io.micronaut.http.MutableHttpResponse
+import io.micronaut.http.HttpStatus.CONFLICT
 import io.micronaut.http.annotation.*
 import io.micronaut.http.uri.UriBuilder
 import io.micronaut.runtime.server.EmbeddedServer
@@ -32,6 +29,12 @@ class AccountController(private val accountService: AccountService,
         return createAccountCommand.flatMap {
             accountService.createAccount(it)
                     .map { acc -> created<Void>(buildAccountCreatedUri(acc.id)) }
+                    .onErrorResumeNext { throwable ->
+                        if (throwable is AccountAlreadyExistsException)
+                            Single.just(HttpResponseFactory.INSTANCE.status<Void>(CONFLICT))
+                        else
+                            Single.error(throwable)
+                    }
         }
     }
 
